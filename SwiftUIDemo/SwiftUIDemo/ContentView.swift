51155479f5d8500bb4b979f60dc36d0ec5c51c93
Tiffany Obi <tiffanyobi-pursuit@Tiffanys-MacBook-Pro.local>
2019-09-24 15:51:55 -0400

    added xcode project

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Tiffany Obi on 9/24/19.
+//  Copyright © 2019 Tiffany Obi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome iOS 6.3")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
